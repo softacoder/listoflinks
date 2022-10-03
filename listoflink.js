25c3fa6f43ecfcd6c3b8c85bd2d1a940edebcc31
softacoder <113185323+softacoder@users.noreply.github.com>
2022-10-03 14:34:58 +0100

    Add files via upload

---
+++ listoflink.js
@@ -0,0 +1,24 @@
+let element_list = document.getElementById('list');
+let new_li = document.createElement('li');
+let new_a = document.createElement('a');
+new_a.innerText = 'https://codeyourfuture.com/';
+
+new_li.appendChild(new_a);
+
+element_list.appendChild(new_li);
+let element_list2 = document.getElementById('list');
+let new_li2 = document.createElement('li');
+let new_a2 = document.createElement('a');
+new_a2.innerText = 'https://www.youtube.com/';
+
+new_li2.appendChild(new_a2);
+
+element_list2.appendChild(new_li2);
+let element_list3 = document.getElementById('list');
+let new_li3 = document.createElement('li');
+let new_a3 = document.createElement('a');
+new_a3.innerText = 'https://twitter.com/';
+
+new_li3.appendChild(new_a3);
+
+element_list3.appendChild(new_li3);
